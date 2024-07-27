8561a372778d56dc09ab1483307e0bb46b04c9a5
Soumo275 <soumostudy27@gmail.com>
2024-07-27 15:11:03 +0530

    payments

---
+++ payment gateway/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const Razorpay = require('razorpay');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const razorpay = new Razorpay({
+    key_id: 'rzp_test_18IDwtqFd2UeDl',
+    key_secret: 'jP5jUVTGv09AAyDnQHV6P6bw',
+});
+
+
+
+app.post('/create-order', async (req, res) => {
+  const { amount, currency } = req.body;
+
+  const options = {
+    amount: amount * 100, // amount in the smallest currency unit
+    currency,
+  };
+
+  try {
+    const order = await razorpay.orders.create(options);
+    res.json(order);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
+  }
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));

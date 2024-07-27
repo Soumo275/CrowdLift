61e4b03c680a736f94dd096c3773c0091bf93ab0
Prabuddha747 <prabuddha331@gmail.com>
2024-07-27 16:57:52 +0530

    frontend

---
+++ Foss_main_page/README.md
@@ -0,0 +1,17 @@
+
+# Social Media Clone
+
+A cool ui/ux social media app using HTML, CSS & Js.
+
+
+## Features
+
+- Cool UI
+- Coustomize color and front
+
+
+## Acknowledgements
+
+ - [EGATOR Yt](https://www.youtube.com/@EGATORTUTORIALS)
+
+
---
+++ Foss_main_page/main.js
@@ -0,0 +1,235 @@
+//Sidebar
+const menuItems = document.querySelectorAll('.menu-item');
+//Messages
+const messagesNotification = document.querySelector('#messages-notifications');
+const messages = document.querySelector('.messages');
+const message = messages.querySelectorAll('.message');
+const messageSearch = document.querySelector('#message-search');
+//Theme
+const theme = document.querySelector('#theme');
+const themeModal = document.querySelector('.customize-theme');
+const fontSizes = document.querySelectorAll('.choose-size span');
+const root = document.querySelector(':root');
+const colorPalette = document.querySelectorAll('.choose-color span');
+
+const Bg1 = document.querySelector('.bg-1');
+const Bg2 = document.querySelector('.bg-2');
+const Bg3 = document.querySelector('.bg-3');
+
+
+//  ----- Notifications -----
+const changeActiveItem = ()=>{
+    menuItems.forEach(item => {
+        item.classList.remove('active');
+    })
+}
+
+menuItems.forEach(item => {
+    item.addEventListener('click', ()=> {
+        changeActiveItem();
+        item.classList.add('active');
+
+        if(item.id != 'Notifications'){
+            document.querySelector('.notifications-popup').style.display = 'none';
+            document.querySelector('.notification-count').style.display = 'block';
+        }else{
+            document.querySelector('.notifications-popup').style.display = 'block';
+            document.querySelector('.notification-count').style.display = 'none';
+        }
+    })
+})
+
+//  ----- Messages -----
+const searchMessage = ()=>{
+    const value = messageSearch.value.toLowerCase();
+    console.log(value)
+    message.forEach(chat => {
+        let name = chat.querySelector('h5').textContent.toLowerCase();
+        if(name.indexOf(value) != -1){
+            chat.style.display = 'flex';
+        }else{
+            chat.style.display = 'none';
+        }
+    })
+}
+
+messageSearch.addEventListener('keyup', searchMessage);
+
+messagesNotification.addEventListener('click',()=> {
+    messages.style.boxShadow = '0 0 1rem var(--color-primary)';
+    messagesNotification.querySelector('.notification-count').style.display = 'none';
+    setTimeout(()=>{
+        messages.style.boxShadow = 'none';
+    }, 2000);
+})
+
+//  ----- ThemeModel -----
+const openThemeModel = ()=>{
+    themeModal.style.display = 'grid';
+}
+
+const closeThemeModal = (e)=>{
+    if(e.target.classList.contains('customize-theme')){
+        themeModal.style.display = 'none';
+    }
+}
+
+themeModal.addEventListener('click', closeThemeModal);
+theme.addEventListener('click', openThemeModel);
+
+// ----- Font size -----
+const removeSizeSelector = () => {
+    fontSizes.forEach(size => {
+        size.classList.remove('active');
+    })
+}
+
+fontSizes.forEach(size => {
+
+    size.addEventListener('click', ()=> {
+        removeSizeSelector();
+        let fontSize;
+        size.classList.toggle('active');
+
+        if(size.classList.contains('font-size-1')){
+            fontSize = '10px';
+            root.style.setProperty('--sticky-top-left', '5.4rem');
+            root.style.setProperty('--sticky-top-right', '5.4rem');
+        }else if(size.classList.contains('font-size-2')){
+            fontSize = '13px';
+            root.style.setProperty('--sticky-top-left', '5.4rem');
+            root.style.setProperty('--sticky-top-right', '-7rem');
+        }else if(size.classList.contains('font-size-3')){
+            fontSize = '16px';
+            root.style.setProperty('--sticky-top-left', '-2rem');
+            root.style.setProperty('--sticky-top-right', '-17rem');
+        }else if(size.classList.contains('font-size-4')){
+            fontSize = '19px';
+            root.style.setProperty('--sticky-top-left', '-5rem');
+            root.style.setProperty('--sticky-top-right', '-25rem');
+        }else if(size.classList.contains('font-size-5')){
+            fontSize = '22px';
+            root.style.setProperty('--sticky-top-left', '-12rem');
+            root.style.setProperty('--sticky-top-right', '-35rem');
+        }
+        //change font size of the root html
+        document.querySelector('html').style.fontSize = fontSize;
+    })
+})
+
+//change the primary color
+const changeActive = () =>{
+    colorPalette.forEach(colorPicker => {
+        colorPicker.classList.remove('active');
+    })
+}
+
+colorPalette.forEach(color => {
+    color.addEventListener('click', () => {
+        let primaryHue;
+        changeActive();
+        
+        if(color.classList.contains('color-1')){
+            primaryHue = 252;
+        }else if(color.classList.contains('color-2')){
+            primaryHue = 52;
+        }else if(color.classList.contains('color-3')){
+            primaryHue = 352;
+        }else if(color.classList.contains('color-4')){
+            primaryHue = 152;
+        }else if(color.classList.contains('color-5')){
+            primaryHue = 202;
+        }
+        color.classList.add('active');
+
+        root.style.setProperty('--color-primary-hue', primaryHue);
+    })
+})
+
+// ---- Theme background -----
+let lightColorLightness;
+let whiteColorLightness;
+let darkColorLightness;
+
+const changeBG = () =>{
+    root.style.setProperty('--light-color-lightness', lightColorLightness);
+    root.style.setProperty('--white-color-lightness', whiteColorLightness);
+    root.style.setProperty('--dark-color-lightness', darkColorLightness);
+} 
+
+Bg1.addEventListener('click', ()=> {
+    Bg1.classList.add('active');
+    Bg2.classList.remove('active');
+    Bg3.classList.remove('active');
+
+    window.location.reload();
+})
+
+Bg2.addEventListener('click', ()=> {
+    darkColorLightness = '95%';
+    whiteColorLightness = '20%';
+    lightColorLightness = '15%';
+
+    Bg2.classList.add('active');
+    Bg1.classList.remove('active');
+    Bg3.classList.remove('active');
+
+    changeBG();
+})
+
+Bg3.addEventListener('click', ()=> {
+    darkColorLightness = '95%';
+    whiteColorLightness = '10%';
+    lightColorLightness = '0%';
+
+    Bg3.classList.add('active');
+    Bg1.classList.remove('active');
+    Bg2.classList.remove('active');
+
+    changeBG();
+})
+
+
+
+
+// TIME-------------------------------------------------------------
+let startTime = Date.now();
+
+function updateTimer() {
+    let currentTime = Date.now();
+    let elapsedTime = Math.floor((currentTime - startTime) / 1000);
+    
+    let days = Math.floor(elapsedTime / (60 * 60 * 24));
+    let hours = Math.floor((elapsedTime % (60 * 60 * 24)) / (60 * 60));
+    let minutes = Math.floor((elapsedTime % (60 * 60)) / 60);
+
+    let displayTime = "";
+
+    if (days > 0) {
+        displayTime = `${days} days`;
+    } else if (hours > 2) {
+        displayTime = `${hours} hours`;
+    } else {
+        displayTime = `${minutes} minutes`;
+    }
+
+    document.getElementById('time').innerText = displayTime;
+}
+
+setInterval(updateTimer, 1000);
+
+
+// DROP BOX JS----------------------------------------------------------------
+function allowDrop(ev) { 
+    ev.preventDefault(); 
+} 
+
+function drag(ev) { 
+    ev.dataTransfer.setData("text", ev.target.id); 
+} 
+
+function drop(ev) { 
+    ev.preventDefault(); 
+    var data = ev.dataTransfer.getData("text"); 
+    ev.target.appendChild(document.getElementById(data)); 
+} 
